b6bf0e634c2de0075ada87c2a959bd9d2390c832
Nikolay Fefilov <astra30.n1k@gmail.com>
2019-08-17 20:00:15 +0400

    Added Console.WriteLine();

--- MyFirstProgram/Program.cs
+++ MyFirstProgram/Program.cs
@@ -10,6 +10,8 @@ namespace MyFirstProgram
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("MyFirstProject");
+            Console.ReadKey();
         }
     }
 }
